9a3490cdc9cb7a6d8bada6ca9196c354ae1a4505
NK-S-cbs <nsakdapipanich26@cranbrook.edu>
2026-03-11 17:01:03 -0400

    localhost + sql

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const pool = new Pool({
   user: 'postgres',
